a6b93cd3e5c9b3eea8a70a2b47398b119082f1d0
turik777 <52492191+turik777@users.noreply.github.com>
2025-07-12 22:29:48 +0300

    feat: add result component

---
+++ src/components/Result/Result.tsx
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+import type { Character } from '../../utils/interface';
+import styles from './result.module.scss';
+
+interface IProps {
+  result: Character[];
+  loading: boolean;
+}
+
+class Result extends Component<IProps> {
+  render() {
+    const { result } = this.props;
+
+    return (
+      <div className={styles.result}>
+        {result.length === 0 ? (
+          <div className={styles.null}>No results</div>
+        ) : (
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th className={styles['header-cell']}>Name</th>
+                <th className={styles['header-cell']}>Description</th>
+              </tr>
+            </thead>
+            <tbody className={styles['table-body']}>
+              {result.map(({ id, name, description }) => (
+                <tr className={styles.row} key={id}>
+                  <td className={`${styles.cell} ${styles.name}`}>{name}</td>
+                  <td className={styles.cell}>{description}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Result;
