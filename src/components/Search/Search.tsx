d152fca0a83be1b86eee310d8af658d84ca5608a
turik777 <52492191+turik777@users.noreply.github.com>
2025-07-12 21:43:45 +0300

    feat: add search component with localStorage saving

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import styles from './search.module.scss';
+import Button from '../Button/Button';
+
+interface IState {
+  query: string;
+}
+
+interface IProps {
+  search: (query: string) => void;
+}
+
+class Search extends Component<IProps, IState> {
+  state: IState = {
+    query: localStorage.getItem('search') || '',
+  };
+
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ query: event.target.value });
+  };
+
+  handleSearch = () => {
+    const { query } = this.state;
+    localStorage.setItem('search', query);
+    this.props.search(query);
+  };
+
+  render() {
+    return (
+      <div className={styles.search}>
+        <input
+          className={styles['search-bar']}
+          type="text"
+          value={this.state.query}
+          onChange={this.handleInputChange}
+        />
+        <Button color="primary" onClick={this.handleSearch}>
+          Search
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default Search;
