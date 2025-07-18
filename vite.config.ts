38a7833483823229a95b6054a2db051e253b5095
turik777 <52492191+turik777@users.noreply.github.com>
2025-07-18 23:49:43 +0300

    chore: add vitest and react test library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/interface',
+        'src/**/*.d.ts',
+        'src/*.tsx',
+      ],
+    },
+  },
 });
