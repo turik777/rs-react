f8eb38202ab0252dfe348948351425cc5aea76ff
turik777 <52492191+turik777@users.noreply.github.com>
2025-07-23 21:44:06 +0300

    refactor: convert card class component to functional component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,39 +1,36 @@
-import { Component } from 'react';
+import type { FC } from 'react';
 import type { Character } from '../../interface/interface';
 import styles from './card.module.scss';
 
-class Card extends Component<Character> {
-  render() {
-    const { image, name, species, gender, status } = this.props;
-    return (
-      <div className={styles.card}>
-        <img className={styles.image} src={image} alt="" />
-        <p className={styles.name}>{name}</p>
-        <div className={styles.info}>
-          <span>Species: </span>
-          <span>{species}</span>
-        </div>
-        <div className={styles.info}>
-          <span>Gender: </span>
-          <span>{gender}</span>
-        </div>
-        <div className={styles.info}>
-          <span>Status: </span>
-          <span
-            className={
-              status === 'Alive'
-                ? styles.alive
-                : status === 'Dead'
-                  ? styles.dead
-                  : styles.unknown
-            }
-          >
-            {` ${status.toUpperCase()}`}
-          </span>
-        </div>
+const Card: FC<Character> = ({ image, name, species, gender, status }) => {
+  return (
+    <div className={styles.card}>
+      <img className={styles.image} src={image} alt="" />
+      <p className={styles.name}>{name}</p>
+      <div className={styles.info}>
+        <span>Species: </span>
+        <span>{species}</span>
       </div>
-    );
-  }
-}
+      <div className={styles.info}>
+        <span>Gender: </span>
+        <span>{gender}</span>
+      </div>
+      <div className={styles.info}>
+        <span>Status: </span>
+        <span
+          className={
+            status === 'Alive'
+              ? styles.alive
+              : status === 'Dead'
+                ? styles.dead
+                : styles.unknown
+          }
+        >
+          {` ${status.toUpperCase()}`}
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
