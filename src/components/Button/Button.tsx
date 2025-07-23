162a1197c22dba686d19b10c5861438a310a5765
turik777 <52492191+turik777@users.noreply.github.com>
2025-07-23 21:39:18 +0300

    refactor: convert button class component to functional component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,19 @@
-import { Component } from 'react';
+import type { FC } from 'react';
 import styles from './button.module.scss';
 
 interface IProps {
   children: string;
   color: 'primary' | 'error';
-  onClick: () => void;
+  onClick?: () => void;
+  disabled?: boolean;
 }
 
-class Button extends Component<IProps> {
-  render() {
-    const { children, color, onClick } = this.props;
-
-    return (
-      <button className={`${styles.button} ${styles[color]}`} onClick={onClick}>
-        {children}
-      </button>
-    );
-  }
-}
+const Button: FC<IProps> = ({ children, color, onClick }) => {
+  return (
+    <button className={`${styles.button} ${styles[color]}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
