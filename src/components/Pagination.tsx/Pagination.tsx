05c7756a8562ee2e4214c05e315cf9c33deb7266
turik777 <52492191+turik777@users.noreply.github.com>
2025-07-25 03:56:25 +0300

    feat: add pagination component

---
+++ src/components/Pagination.tsx/Pagination.tsx
@@ -0,0 +1,33 @@
+import type { FC } from 'react';
+import Button from '../Button/Button';
+import styles from './pagination.module.scss';
+
+interface IProps {
+  page: number;
+  pageChange: (page: number) => void;
+  totalPages: number;
+}
+
+const Pagination: FC<IProps> = ({ page, pageChange, totalPages }) => {
+  return (
+    <div className={styles.pagination}>
+      <Button
+        color="primary"
+        onClick={() => pageChange(page - 1)}
+        disabled={page <= 1}
+      >
+        {'<'}
+      </Button>
+      <span>{page}</span>
+      <Button
+        color="primary"
+        onClick={() => pageChange(page + 1)}
+        disabled={page >= totalPages}
+      >
+        {'>'}
+      </Button>
+    </div>
+  );
+};
+
+export default Pagination;
