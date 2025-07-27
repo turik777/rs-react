1e8a28f897edff2d604674e8e26446b3ca1167c1
turik777 <52492191+turik777@users.noreply.github.com>
2025-07-28 00:57:28 +0300

    feat: add tests for details component

---
+++ src/components/Details/Details.test.tsx
@@ -0,0 +1,34 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+import Details from './Details';
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router';
+
+const renderWithRouter = (ui: React.ReactElement) =>
+  render(<BrowserRouter>{ui}</BrowserRouter>);
+
+const renderWithMemory = (ui: React.ReactElement, entry: string) =>
+  render(<MemoryRouter initialEntries={[entry]}>{ui}</MemoryRouter>);
+
+describe('Details Component', () => {
+  it('should not render when no id is present', () => {
+    const { container } = renderWithRouter(<Details />);
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it('load and display character by id from url search param', async () => {
+    renderWithMemory(
+      <Routes>
+        <Route path="/" element={<Details />} />
+      </Routes>,
+      '/?details=1'
+    );
+    await waitFor(() => {
+      expect(screen.getByText('Rick Sanchez')).toBeInTheDocument();
+      expect(screen.getByText('Species:')).toBeInTheDocument();
+      expect(screen.getByText('Gender:')).toBeInTheDocument();
+      expect(screen.getByText('Status:')).toBeInTheDocument();
+      expect(screen.getByText('Close')).toBeInTheDocument();
+    });
+  });
+});
