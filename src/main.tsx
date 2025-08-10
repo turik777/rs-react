13af6acc4e1ebbe163619776793284b1e03cd11c
turik777 <52492191+turik777@users.noreply.github.com>
2025-08-10 05:25:43 +0300

    feat: add tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -6,24 +6,35 @@ import About from './components/About/About.tsx';
 import NotFound from './components/NotFound/NotFound.tsx';
 import Details from './components/Details/Details.tsx';
 import { ThemeProvider } from './context/ThemeProvider.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const container = document.getElementById('root');
 if (!container) {
   throw new Error('Missing container');
 }
 
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30 * 60 * 1000,
+    },
+  },
+});
+
 createRoot(container).render(
-  <ThemeProvider>
-    <BrowserRouter>
-      <StrictMode>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<Details />} />
-          </Route>
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </StrictMode>
-    </BrowserRouter>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+      <BrowserRouter>
+        <StrictMode>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<Details />} />
+            </Route>
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </StrictMode>
+      </BrowserRouter>
+    </ThemeProvider>
+  </QueryClientProvider>
 );
