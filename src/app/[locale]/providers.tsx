1e3cf67c7a4e806225138afc1397eb85f0be4708
turik777 <52492191+turik777@users.noreply.github.com>
2025-08-17 22:37:34 +0300

    refactor: move query client to its own module

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -2,15 +2,8 @@
 
 import { useEffect, useState, type FC, type ReactNode } from 'react';
 import { ThemeContext, type Theme } from '../../context/ThemeContext';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 30 * 60 * 1000,
-    },
-  },
-});
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '../../utils/queryClient';
 
 export const Providers: FC<{ children: ReactNode }> = ({ children }) => {
   const [theme, setTheme] = useState<Theme>('light');
